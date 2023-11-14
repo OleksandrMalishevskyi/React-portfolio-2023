42381bc5a6465a081f9354e00edbfc73093a3365
TechnoTiger <oleksandr.malishevskiy@gmail.com>
2023-11-14 15:24:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+
+// import Layout from './components/Layout/Layout'
+
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import './App.scss'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <Sidebar />
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Routes>
+            {/* <Route path="/" element={<Layout />} /> */}
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
