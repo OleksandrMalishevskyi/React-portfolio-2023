462361d071fc19afa140a611682a6b7ddee6bdfc
TechnoTiger <oleksandr.malishevskiy@gmail.com>
2023-11-15 22:17:05 +0100

    add portfolio and contact page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-
-// import Layout from './components/Layout/Layout'
-
+import Sidebar from './components/Sidebar/Sidebar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from './components/Contact/Contact'
 import './App.scss'
-import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
@@ -14,11 +13,14 @@ function App() {
         <Sidebar />
         <div className="page">
           <span className="tags top-tags">&lt;body&gt;</span>
+
           <Routes>
-            {/* <Route path="/" element={<Layout />} /> */}
             <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
+
           <span className="tags bottom-tags">
             &lt;/body&gt;
             <br />
